1ac4ceb2b700b2d9d4f9be3dc26f1d5d305942e0
Ahadu Kebede <ahaduk@archyoga.localdomain>
2021-04-15 19:33:44 -0400

    Version 0.1. Added basic functionallity to tweet lines of text.

---
+++ src/userInfo.java
@@ -0,0 +1,6 @@
+public class userInfo {
+   public String accessTokenSecret = ""; //enter API keys here.
+   public String accessToken = "";
+   public String consumerKey = "";
+   public String consumerKeySecret = "";
+}
