00c1d3367daecf3f5fa1e4488771a23e563a1c53
Ahadu Kebede <ahaduk@archyoga.localdomain>
2021-04-18 11:07:17 -0400

    Added the abiity to view tweets on the timeline

--- src/main/java/TUItter/App.java
+++ src/main/java/TUItter/App.java
@@ -1,10 +1,13 @@
 package TUItter;
 
+import java.util.List;
 import java.util.Scanner;
 
+import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
+import twitter4j.User;
 import twitter4j.conf.ConfigurationBuilder;
 
 public class App 
@@ -25,7 +28,7 @@ public class App
                 cTweet();
                 break;
             case 2:
-            System.out.println("Coming Soon!");
+                viewTimeline();
             break;
             default:
             System.out.println("Invalid Choice!");
@@ -46,7 +49,7 @@ public class App
         Twitter twitter = tf.getInstance();
 
         while(true){
-            System.out.print("Enter tweet:");
+            System.out.print("Enter tweet: ");
             tweet = input.nextLine();
                 switch(tweet){
                     case "EXIT":
@@ -62,6 +65,31 @@ public class App
 
                 }
         }
-
     }
+    public static void viewTimeline(){
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        userInfo user = new userInfo();
+        cb.setDebugEnabled(true)
+            .setOAuthConsumerKey(user.consumerKey)
+            .setOAuthConsumerSecret(user.consumerKeySecret)
+            .setOAuthAccessToken(user.accessToken)
+            .setOAuthAccessTokenSecret(user.accessTokenSecret);
+            TwitterFactory tf = new TwitterFactory(cb.build());
+
+            Twitter twitter = tf.getInstance();
+            try {
+                //initializes user with creditials from userInfo class.
+                User userC = twitter.verifyCredentials();
+                List<Status> stati = twitter.getHomeTimeline(); //list of tweets from home timeline, also the plural of status is stati right?
+                System.out.printf("Showing tweets from @%s's timeline", userC.getScreenName());
+                System.out.println("\n----------------------------------------------------");
+                for (Status status : stati) {
+                    System.out.println("@" + status.getUser().getScreenName() + " - " + status.getText() + "");
+                }
+            } catch (TwitterException te) {
+                te.printStackTrace();
+                System.out.printf("Failed to get timeline: " + te.getMessage());
+                System.exit(-1);
+            }
+        }
 }
--- src/main/java/TUItter/userInfo.java
+++ src/main/java/TUItter/userInfo.java
@@ -1,10 +1,8 @@
 package TUItter;
 
 public class userInfo {
-   //Enter API keys here.
-   public String accessTokenSecret = "";
-   public String accessToken = "";
-   public String consumerKey = "";
-   public String consumerKeySecret = "";
-        
+   public String accessTokenSecret = "vrA3qIsyGVJilKnPRJSnrj5rXXGTdVz08NNEi4xSfmqOp"; //enter API keys here.
+   public String accessToken = "1369100309683912709-2dNmPoM7i2Se6rZFcMo6QRfWPlhHoc";
+   public String consumerKey = "tGDikRW23hdYHOnvHaJy6SurO";
+   public String consumerKeySecret = "ODTy2sZQGma4gR0PlP7aU4imkmumbHmWxHeZuvK5my4dQtYRi3";
 }
